82cbc87742c25aeea4b04f92814e46898aee9c77
Jasper Malfait <jasper.malfait@ugent.be>
2021-03-26 16:05:40 +0100

    Initial commit

---
+++ MediaPickerSample/MediaPickerSample/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tesseract;
+
+namespace MediaPickerSample
+{
+    public interface Test
+    {
+        ITesseractApi GetTesseractApi();
+    }
+}
